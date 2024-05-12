886de31118b4f7a4860b4f1212302137bd9903e8
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-05-12 18:44:50 +0900

    update weapon files

---
+++ Assets/Weapons/katana.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Katana : MonoBehaviour
+{
+    public float attackSpeed = 2.0f; 
+    public float moveSpeed = 6.0f; 
+    public float range = 2.0f; 
+    public int damage = 60; 
+
+}
