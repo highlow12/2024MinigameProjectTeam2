72791469eff71130a37d215e67463e8ec680e8db
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-05-12 18:44:50 +0900

    update weapon files

---
+++ Assets/Weapons/Bow.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using items;
+public class Bow : items.item
+{
+    public float attackSpeed = 1.0f; 
+    public float moveSpeed = 5.0f; 
+    public float projectileSpeed = 10.0f; 
+    public float range = 20.0f; 
+    public int damage = 50; 
+
+}
