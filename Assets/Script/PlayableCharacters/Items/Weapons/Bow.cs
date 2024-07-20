0a8d997e1ad338097f80ee31f1c60b2eab4b82a1
Bonobo0 <kjh893197@naver.com>
2024-07-21 00:34:23 +0900

    Use animator parameter to change animation speed instead of changing animator speed directly
    in progress

--- Assets/Script/PlayableCharacters/Items/Weapons/Bow.cs
+++ Assets/Script/PlayableCharacters/Items/Weapons/Bow.cs
@@ -4,8 +4,10 @@ using Items;
 
 public class Bow : Weapon
 {
+    private bool isAttackCooldown = false;
     public Bow(float attackSpeed)
     {
+        this.attackSpeed = attackSpeed;
         projectileSpeed = 10.0f;
         range = 20.0f;
         damage = 50;
@@ -13,8 +15,40 @@ public class Bow : Weapon
 
     public override IEnumerator Attack(Animator anim, Transform character)
     {
-        Debug.Log("Firing Arrow");
-        yield return new WaitForSeconds(attackSpeed);
+
+        if (!isAttackCooldown)
+        {
+            // 0.5f = animation length
+            // 0.3f = combo delay
+
+            if (attackState == 3)
+            {
+                attackState = 0;
+            }
+            attackState++;
+            // 애니메이션 배속
+            anim.SetFloat("AttackAnimSpeed", 0.5f * attackSpeed);
+            anim.SetInteger("AttackState", attackState);
+            prevAttack = Time.time;
+            anim.SetFloat("PrevAttack", prevAttack);
+            anim.SetTrigger("Attack");
+            anim.SetBool("Combo", true);
+            yield return new WaitForSeconds(0.1f);
+            isAttackCooldown = true;
+            yield return new WaitForSeconds(1.0f / attackSpeed - 0.1f);
+            isAttackCooldown = false;
+            yield return new WaitForSeconds(0.3f);
+            if (Time.time - prevAttack > 1.0f / attackSpeed + 0.3f)
+            {
+                anim.SetInteger("AttackState", 0);
+                anim.SetBool("Combo", false);
+                attackState = 0;
+            }
+        }
+        else
+        {
+            yield return null;
+        }
     }
 
 }
