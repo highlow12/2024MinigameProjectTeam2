01ff4a377b2577f8d7a90541dda47450cbe26565
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-07-27 23:25:09 +0900

    MakeRoom&SceneLoading

--- Assets/Script/Matchmaking&SelectRoom/MatchMaking.cs
+++ Assets/Script/Matchmaking&SelectRoom/MatchMaking.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MatchMaking : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NetworkRunner _runner;
+    [SerializeField] string sceneName = "BossMonsterTest.unity";
+    [SerializeField] TMP_InputField sessionName;
+
+    async void StartGame(GameMode mode)
     {
-        
+        // Create the Fusion runner and let it know that we will be providing user input
+        _runner = gameObject.AddComponent<NetworkRunner>();
+        _runner.ProvideInput = true;
+
+        // Create the NetworkSceneInfo from the current scene
+        var scene = SceneRef.FromIndex(SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/" + sceneName));
+        var sceneInfo = new NetworkSceneInfo();
+        if (scene.IsValid)
+        {
+            sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
+        }
+
+        // Start or join (depends on gamemode) a session with a specific name
+        await _runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = sessionName.text,
+            Scene = scene,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void STARTGAME_host() { StartGame(GameMode.Host); }
+    public void STARTGAME_client() { StartGame(GameMode.Client); }
 }
