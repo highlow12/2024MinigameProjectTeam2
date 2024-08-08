d1532bb483cc8601be764d52d5c476f0f7410751
Bonobo0 <kjh893197@naver.com>
2024-08-09 02:58:43 +0900

    BossHitFeedbackEffect
    work in progress

--- Assets/Script/BossMonster/BossHitFeedbackEffect.cs
+++ Assets/Script/BossMonster/BossHitFeedbackEffect.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
 using UnityEngine;
 
-public class BossHitFeedbackEffect : MonoBehaviour
+public class BossHitFeedbackEffect : NetworkBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
