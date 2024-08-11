a32a3ea7766f9015cd200210990c77e7303e19e8
Bonobo0 <kjh893197@naver.com>
2024-08-11 23:09:23 +0900

    Boss balancing

--- Assets/Script/BossMonster/BossMonsterNetworked.cs
+++ Assets/Script/BossMonster/BossMonsterNetworked.cs
@@ -404,7 +404,7 @@ public class BossMonsterNetworked : NetworkBehaviour
         if (bossCondition.HasFlag(Condition.IsPlayerInFar))
         {
             conditionDuration += Time.fixedDeltaTime;
-            if (conditionDuration >= 3.0f)
+            if (conditionDuration >= 2.0f)
             {
                 attackType = AttackType.JumpDash;
                 CurrentState = BossState.Attack;
@@ -433,7 +433,7 @@ public class BossMonsterNetworked : NetworkBehaviour
             if (CurrentState == BossState.Attack && isAttacking == true)
             {
                 Debug.Log("Set timer");
-                BossAttackTimer = CustomTickTimer.CreateFromSeconds(Runner, Random.Range(2.0f, 3.5f));
+                BossAttackTimer = CustomTickTimer.CreateFromSeconds(Runner, Random.Range(1.5f, 2.0f));
             }
         }
         UpdateAttribute();
