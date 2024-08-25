f8faca240527ca068ff3042ba3e24097f192a14f
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-08-25 14:09:29 +0900

    Make script prototype

--- Assets/Script/BossMonster/Skills/Phase2/Shunpo.cs
+++ Assets/Script/BossMonster/Skills/Phase2/Shunpo.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 
 public class Shunpo : BossSkill
 {
-    public override IEnumerator Attack(Transform transform, Animator animator, NetworkRunner runner, BossAttack bossAttack = null, NetworkObject projectile = null, NetworkObject boss = null)
+    public float WaitTime = 3;
+    Shunpo()
     {
+        name = "Shunpo";
+        phase = 2;
+        attackDamage = 10;
+    }
+
+    public override IEnumerator Attack(Transform transform, Animator animator, NetworkRunner runner, BossAttack bossAttack = null, NetworkObject projectile = null/*<-this is sword*/, NetworkObject boss = null)
+    {
+        if (projectile.TryGetComponent<Phase2Sword>(out var sword))
+        {
+            var target = boss.GetComponent<BossMonsterNetworked>().FollowTarget;
+            sword.ThrowSword(target.transform.position);
+            yield return new WaitForSeconds(WaitTime);
+            transform.position = sword.transform.position;
+        }
         yield return null;
     }
 
