30e20b56a9b027203fb43baf651a9b0efc3d8612
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-08-26 21:54:25 +0900

    Make SpawnFire Prototype

--- Assets/Script/BossMonster/Projectiles/Phase2/FireSpirit.cs
+++ Assets/Script/BossMonster/Projectiles/Phase2/FireSpirit.cs
@@ -9,7 +9,14 @@ public class FireSpirit : NetworkBehaviour
     Vector2 EndPoint = Vector2.left;
     Vector2 TargetPos = new Vector2(0, 0);
     float currentTime = 0;
-    
+    [Networked] private TickTimer life { get; set; }
+    public List<PlayerRef> playersHit = new();
+    public int damage = 10;
+    public float speed = 5;
+    public float lifeSeconds = 3;
+    private int dir = 1;
+    public AudioClip attackClip;
+
     public void Awake()
     {
         currentTime = 0;
@@ -46,6 +53,38 @@ public class FireSpirit : NetworkBehaviour
         {
             //downward curve
             transform.position = GetBesierPosition(EndPoint, TargetPos, Vector2.down, currentTime - 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerControllerNetworked player = other.gameObject.GetComponent<PlayerControllerNetworked>();
+            if (player)
+            {
+                if (playersHit.Contains(player.Player))
+                {
+                    return;
+                }
+                playersHit.Add(player.Player);
+                if (player.CharacterClass == (int)CharacterClassEnum.Tank)
+                {
+                    if (player.weapon.isDraw)
+                    {
+                        player.Skill();
+                        Runner.Despawn(Object);
+                        SFXManager.Instance.playSFX(attackClip);
+                        return;
+                    }
+                }
+                BossAttack.AttackData attackData = new()
+                {
+                    damage = damage
+                };
+                player.RPC_OnPlayerHit(attackData);
+
+            }
         }
     }
 }
--- Assets/Script/BossMonster/Skills/Phase2/SpawnFireSpirit.cs
+++ Assets/Script/BossMonster/Skills/Phase2/SpawnFireSpirit.cs
@@ -1,18 +1,24 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Fusion.Sockets.NetBitBuffer;
 
-public class SpawnFireSpirit : MonoBehaviour
+public class SpawnFireSpirit : BossSkill
 {
-    // Start is called before the first frame update
-    void Start()
+    public override IEnumerator Attack(Transform transform, Animator animator, NetworkRunner runner, BossAttack bossAttack = null, NetworkObject projectile = null, NetworkObject boss = null)
     {
-        
-    }
+        runner.Spawn(projectile,
+        transform.position, Quaternion.Euler(0, 0, 0),
+            boss.InputAuthority, (runner, o) =>
+            {
+                
+                var s = o.GetComponent<FireSpirit>();
+                o.transform.parent = transform;
+                s.setTarget(transform.GetComponent<BossMonsterNetworked>().FollowTarget.transform.position);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            }
+        );
+        yield return null;
     }
 }
